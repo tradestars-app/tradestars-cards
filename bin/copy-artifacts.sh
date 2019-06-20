4fd338575b9f015828ac9fdb4fdc789213f8419c
Christian Hentschel <chentschel@gmail.com>
2019-06-20 15:04:15 -0300

    fix: config files

--- bin/copy-artifacts.sh
+++ bin/copy-artifacts.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+APP_PATH="../../tradestars-ui/src/artifacts"
+
 # Delete any old artifacts from the app's directory
-rm -f ../tradestars/app/js/artifacts/*
+rm -f $APP_PATH/*
 
 # Copy the newly compiled artifacts to the app's directory
 cp build/contracts/PerformanceCard.json \
     build/contracts/PerformanceCollection.json \
-    build/contracts/BondedERC20.json ../tradestars/app/js/artifacts/
+    build/contracts/BondedERC20.json \
+    $APP_PATH
