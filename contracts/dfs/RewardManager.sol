4288f0e20c77005de791859b5cf32ad275d070b3
Christian Hentschel <chentschel@gmail.com>
2022-07-06 18:18:36 +0400

    fix: typo

--- contracts/dfs/RewardManager.sol
+++ contracts/dfs/RewardManager.sol
@@ -66,7 +66,7 @@ contract RewardManager is OperationManaged, IRewardManager {
     {
         require(
             balancesHash[_beneficiary] >= _amount, 
-            "spendBalance: not enougth balance"
+            "spendBalance: not enought balance"
         );
 
         balancesHash[_beneficiary] -= _amount; 
